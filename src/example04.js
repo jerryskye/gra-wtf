1c8eab353178a9f4fbe803e702e7d115d1ce8c38
jerryskye <jerryskye@bughunt.in>
2018-05-14 12:40:17 +0200

    transparency, material in menu

--- src/example04.js
+++ src/example04.js
@@ -28,10 +28,80 @@ function buildGUI() {
       }
   );
 
+  var CameraZ = function() { this.z = 30; }
+  var cameraZ = new CameraZ();
+  object.add(cameraZ, 'z')
+    .name('Camera z:')
+    .min(0)
+    .max(100)
+    .step(1)
+    .onChange(function(value) {
+      camera.position.z = value;
+    });
+
+  var Transparency = function() { this.transp = 1; }
+  var transparency = new Transparency();
+  object.add(transparency, 'transp')
+    .name('Transparency')
+    .min(0)
+    .max(1)
+    .step(0.01)
+    .onChange(function(value) {
+      mesh.material.opacity = value;
+    });
+
+  var ObjectMaterial = function() { this.material = ""; }
+  var objectMaterial = new ObjectMaterial();
+  object.add(objectMaterial, 'material', ['Wireframe', 'Flat', 'Gourand', 'Phong'])
+    .name('Material type')
+    .onChange(function(value) {
+      var color = mesh.material.color;
+      var transp = mesh.material.transp;
+      var newMaterial;
+      switch(value) {
+        case 'Wireframe':
+          newMaterial = new THREE.MeshBasicMaterial({
+            color,
+            transparent: true,
+            opacity: transp,
+            wireframe: true});
+          break;
+        case 'Flat':
+          newMaterial = new THREE.MeshPhongMaterial({
+            color,
+            transparent: true,
+            opacity: transp,
+            wireframe: THREE.FlatShading});
+          break;
+        case 'Gourand':
+          newMaterial = new THREE.MeshLambertMaterial({
+            color,
+            transparent: true,
+            opacity: transp,
+            wireframe: THREE.SmoothShading});
+          break;
+        default:
+          newMaterial = new THREE.MeshPhongMaterial({
+            color,
+            transparent: true,
+            opacity: transp,
+            wireframe: THREE.SmoothShading});
+          break;
+      }
+      mesh.material = newMaterial;
+    });
+
+  var Button = function() {
+    this.buttonFunction = function() {
+      alert("You pressed the button.");
+    }
+  }
+  var button = new Button();
+  object.add(button, 'buttonFunction').name('Button');
+
   objectsMenu.open();
 
   var customContainer = document.getElementById('gui');
-  console.log(customContainer);
   customContainer.appendChild(gui.domElement);
 }
 
@@ -41,18 +111,18 @@ export function init() {
   camera.position.z = 30;
   scene = new THREE.Scene();
   mesh = new THREE.Mesh(new THREE.TorusGeometry(10, 1, 16, 30),
-    new THREE.MeshPhongMaterial());
+    new THREE.MeshPhongMaterial({transparent: true}));
     //new THREE.MeshLambertMaterial());
   scene.add(mesh);
 
-  var light = new THREE.PointLight(0xffff00, 1, 500);
+  var light = new THREE.PointLight(0xffffff, 1, 500);
   light.position.set(10, 10, 50);
   scene.add(light);
 
   renderer = new THREE.WebGLRenderer();
   renderer.setPixelRatio(window.devicePixelRatio);
   renderer.setSize(window.innerWidth, window.innerHeight);
-  renderer.setClearColor(0x0000ff, 1);
+  renderer.setClearColor(0xd7d7d7, 1);
 
   buildGUI();
   document.body.appendChild(renderer.domElement);
